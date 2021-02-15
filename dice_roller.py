1d1a94dbc3ec60aaa99eb33c6ee27357fd674a73
ramzyrsr <79071247+ramzyrsr@users.noreply.github.com>
2021-02-15 20:08:19 +0700

    Update dice_roller.py
    Adding conditional if else

--- dice_roller.py
+++ dice_roller.py
@@ -8,7 +8,13 @@ def main():
         global dice_sum
         roll = random.randint(1,6)
         dice_sum += roll
-        print(f'You rolled a {roll}')
+        
+        if roll == 1:
+            print(f'You rolled a {roll}! Critical Fail')
+        elif roll == 6:
+            print(f'You rolled a {roll}! Critical Success')
+        else:
+            print(f'You rolled a {roll}')
 
 print(f'You have rolled a total of {dice_sum}')
 
